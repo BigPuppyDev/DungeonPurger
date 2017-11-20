7e8cc04dfc882dccd1af41e221f3350819839c6d
Patrick Ghosn <patrick.ghosn@foo-solutions.com>
2017-11-20 22:15:48 +0200

    refactoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    public float speed = 1f;
-    
-    private Vector2 nextPosition;
-    private Vector3 nextRotation;
+{    
     public bool isMoving = false;
     public bool isAttacking = false;
+    public float speed = 1f;
+
     private float turnAngle;
+    private Vector2 nextPosition;
+    private Vector2 positionOnScreen;
+    private Vector2 mouseOnScreen;
+    private Vector3 nextRotation;
+        
     private Rigidbody2D rigid;
     private Animator animatorController;
 
     private void Awake()
-    {
-        rigid = GetComponent<Rigidbody2D>();
+    {        
         nextPosition = Vector2.zero;
+        nextRotation = Vector3.zero;
+        rigid = GetComponent<Rigidbody2D>();
         animatorController = GetComponent<Animator>();
     }
     	
@@ -51,23 +55,7 @@ public class PlayerController : MonoBehaviour
     {
         Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
-
-    private void Turn()
-    {
-        //turnAngle = Mathf.Atan2(mouseOnScreen.y, mouseOnScreen.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.AngleAxis(turnAngle, Vector3.forward);
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
-
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints( mouseOnScreen, positionOnScreen);
-
-        //Ta Daaa
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-    }
-
+    
     private void Move(float directionX, float directionY)
     {
         nextPosition.Set(directionX, directionY);
@@ -75,8 +63,17 @@ public class PlayerController : MonoBehaviour
         isMoving = rigid.velocity != Vector2.zero ? true : false;
     }
 
-    private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    private void Turn()
     {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        mouseOnScreen = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        turnAngle = Mathf.Atan2(mouseOnScreen.y - positionOnScreen.y, mouseOnScreen.x - positionOnScreen.x) * Mathf.Rad2Deg;
+        Turn(turnAngle);
+    }
+
+    private void Turn(float angle)
+    {
+        nextRotation.z = angle;
+        transform.rotation = Quaternion.Euler(nextRotation);
     }
 }
